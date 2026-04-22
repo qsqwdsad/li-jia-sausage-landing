e8e88ab2acbdf5072432eeb92bd4996d7a84ada4
qsqwdsad <126803179+qsqwdsad@users.noreply.github.com>
2026-04-22 14:52:08 +0800

    Add RootLayout component and metadata for page

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "花蓮東海岸必吃美食｜李家炭火香腸",
+  description:
+    "來花蓮旅遊必吃！炭火現烤香腸，在地人推薦，東海岸公路經過一定要停。",
+};
+
+export default function RootLayout({ children }: any) {
+  return (
+    <html lang="zh-Hant">
+      <body>{children}</body>
+    </html>
+  );
+}
